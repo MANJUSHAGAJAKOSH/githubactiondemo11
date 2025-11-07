1b4f54c57ddcafad26d5bd7a32257b76fa81802d
MANJUSHAGAJAKOSH <manjushagajakosh1@gmail.com>
2025-11-07 12:20:11 +0530

    pandas

--- extract.py
+++ extract.py
@@ -1,2 +1,14 @@
+import  pandas as pd
+print("Extract.py ")
 
-print("Extract.py file")
+data = {
+    "id": [1,2,3],
+    "name" : ["A", "B", "C"],
+    "age" : [10, 20, 30]
+
+}
+
+df=pd.DataFrame(data)
+print(df)
+
+
